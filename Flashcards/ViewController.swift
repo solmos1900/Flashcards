53a1938fe3bf1d44b4f58fe2f0c7a8785efe3370
Sebastian Olmos <sebastian-olmos@uiowa.edu>
2020-02-09 21:40:39 -0600

    Initial Commit

---
+++ Flashcards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by Sebastian Olmos on 2/9/20.
+//  Copyright © 2020 Sebastian Olmos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
